679968441881f9afe7a8de2cb35d0752ebedec41
Mike MacAdam <mikemacadam87@gmail.com>
2016-10-10 15:14:43 -0700

    assignment 5 done

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,12 @@
 (function() {
      function config($stateProvider, $locationProvider) {
-         $locationProvider
+        $locationProvider
          .html5Mode({
              enabled: true,
              requireBase: false
          });
          
-         $stateProvider
+     $stateProvider
          .state('landing', {
              url: '/',
              controller: 'LandingCtrl as landing',
@@ -14,14 +14,15 @@
          })
          .state('album', {
              url: '/album',
+             controller: 'AlbumCtrl as album',
              templateUrl: '/templates/album.html'
-
          })
          .state('collection', {
              url: '/collection',
              controller: 'CollectionCtrl as collection',
              templateUrl: '/templates/collection.html'
-         })
+         });
+     }
  
      angular
          .module('blocJams', ['ui.router'])
